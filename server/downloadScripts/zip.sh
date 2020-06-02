73daa9c5c3e82078553ad4e1097eff5a0bd724f6
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2020-06-02 10:04:20 -0500

    added basic bash scripts to handle downloads

---
+++ server/downloadScripts/zip.sh
@@ -0,0 +1,4 @@
+cd ..
+cd ..
+cd Deployments
+mkdir Test
--- server/routes/downloads.js
+++ server/routes/downloads.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const { spawn } = require('child_process');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* Handles POST to route */
+router.post('/', function(req, res, next) {
+  spawn('sh', ['zip.sh'], {cwd: './downloadScripts'});
+  res.status(200).end();
 });
 
 module.exports = router;
