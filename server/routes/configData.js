35baf097c633504725b4404c0dd2488a76d83f24
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2020-06-22 16:06:49 -0500

    fixed response status on config routes

--- server/routes/configData.js
+++ server/routes/configData.js
@@ -21,16 +21,16 @@ router.post('/setAuthKey', (req, res) => {
     if (req.body.authKey !== '') {
         const oldConfig = JSON.parse(fs.readFileSync('./config.json'));
         fs.writeFileSync('./config.json', JSON.stringify({ authKey: req.body.authKey, mongoDb: oldConfig.mongoDb, downloadPath: oldConfig.downloadPath }));
-        res.status(200).end();
     }
+    res.status(200).end();
 })
 
 router.post('/setDownloadPath', (req, res) => {
     if (req.body.downloadPath !== '') {
         const oldConfig = JSON.parse(fs.readFileSync('./config.json'));
         fs.writeFileSync('./config.json', JSON.stringify({ authKey: oldConfig.authKey, mongoDb: oldConfig.mongoDb, downloadPath: req.body.downloadPath }));
-        res.status(200).end();
     }
+    res.status(200).end();
 })
 
 module.exports = router;
